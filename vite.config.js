e56a0c961f2cbe01e948420f6ecfb9f37adbdbc7
KovalikVadimdev <124208880+KovalikVadimdev@users.noreply.github.com>
2025-04-26 19:55:30 +0300

    hi

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import fs from "fs";
+import path from "path";
+
+const keyPath = path.resolve("certs", "localhost-key.pem");
+const certPath = path.resolve("certs", "localhost.pem");
 
 export default defineConfig({
   plugins: [react()],
   server: {
     https: {
-      key: fs.readFileSync("./certs/localhost-key.pem"),
-      cert: fs.readFileSync("./certs/localhost.pem"),
+      key: fs.readFileSync(keyPath),
+      cert: fs.readFileSync(certPath),
     },
     host: "localhost",
     port: 5173,
   },
-  base: "/potest/",
+  base: "/testproject/",
 });
