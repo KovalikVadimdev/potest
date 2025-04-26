d0aae853167d7a51a75b7deaacaabb2c391df732
KovalikVadimdev <124208880+KovalikVadimdev@users.noreply.github.com>
2025-04-26 20:05:09 +0300

    hi

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,24 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import fs from "fs";
 import path from "path";
-
-const keyPath = path.resolve("certs", "localhost-key.pem");
-const certPath = path.resolve("certs", "localhost.pem");
+import { VitePluginStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginStaticCopy({
+      targets: [
+        {
+          src: "certs/*",
+          dest: "certs",
+        },
+      ],
+    }),
+  ],
   server: {
     https: {
-      key: fs.readFileSync(keyPath),
-      cert: fs.readFileSync(certPath),
+      key: fs.readFileSync(path.resolve("certs", "localhost-key.pem")),
+      cert: fs.readFileSync(path.resolve("certs", "localhost.pem")),
     },
     host: "localhost",
     port: 5173,
