09e62d29a307abdc72fd2ffe907a918656e513c6
KovalikVadimdev <124208880+KovalikVadimdev@users.noreply.github.com>
2025-04-26 19:09:14 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import fs from "fs";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    https: {
-      key: fs.readFileSync("./certs/localhost-key.pem"),
-      cert: fs.readFileSync("./certs/localhost.pem"),
-    },
-    host: "localhost",
-    port: 5173,
-  },
-  base: "/testproject/",
+  base: "/potest",
 });
